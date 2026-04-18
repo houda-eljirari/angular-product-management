b8e9d763350efc5d3ffe0c9f44f7df3045494060
houda-eljirari <eljirarihouda99@gmail.com>
2026-04-18 17:20:29 +0100

    Setup routing and display product list

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {}
+export class ProductListComponent {
+
+  products: Product[];
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
+}
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Product {}
+export class ProductService {
+
+  private products: Product[] = [
+    { id: 1, name: "PC", price: 8000, quantity: 10 },
+    { id: 2, name: "Téléphone", price: 3000, quantity: 20 },
+    { id: 3, name: "Clavier", price: 200, quantity: 50 }
+  ];
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+}
